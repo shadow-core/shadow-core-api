b8e28af1920abc89275c80415665dc86bcf49475
Gray Hound <admin@devilmaydie.name>
2019-05-16 19:19:22 +0500

    Added nodemon for testing changed code

--- app/app.js
+++ app/app.js
@@ -7,20 +7,23 @@ let bodyParser = require('body-parser');
 let app = express();
 let port = config.port;
 
+//connecting bodyParser to work with JSON
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//this is REST API so there's not need to check for origin or CORS or anything else
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-type,Accept,Authorization');
-    if (req.method == 'OPTIONS') {
+    if (req.method == 'OPTIONS') { //always return 200 for OPTIONS header
         res.status(200).end();
     } else {
         next();
     }
 });
 
+//connect all routes
 require('./router/index').default(app);
 
 //run express
@@ -28,4 +31,5 @@ let server = app.listen(port, function() {
     console.log(`Skindu API is now running at port ${port}`);
 });
 
+//export express
 module.exports = server;
