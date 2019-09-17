2d60f1a0aa905ef23b7a4a81a12f1091ab1cb9e9
Gray Hound <admin@devilmaydie.name>
2019-09-17 11:09:40 +0500

    Add refresh token secret and expiration

--- app/config/config.js
+++ app/config/config.js
@@ -12,6 +12,9 @@ export default {
       'maxVerificationTime': 7 * 24 * 3600 * 1000,
     },
     'auth': {
-      'jwtSecret': process.env.JWT_SECRET || 'J6vg20RP7K7ZGi7G43Pd0ZOyE7c8skRG9VIj73tSh3Tl5I7xl4F5zbHXO3pIKjTo'
+      'jwtSecret': process.env.JWT_SECRET || 'J6vg20RP7K7ZGi7G43Pd0ZOyE7c8skRG9VIj73tSh3Tl5I7xl4F5zbHXO3pIKjTo',
+      'jwtSecretRefresh': process.env.JWT_SECRET_REFRESH || 'kPikXxNQ4e1lKok9W2xKnBIigQ6m2B0SFNxqmolILe0b5ai02Ka6HrETadH6tNJe',
+      'tokenExpiresIn': process.env.TOKEN_EXPIRE || '1 hour',
+      'refreshTokenExpiresIn': process.env.REFRESH_TOKEN_EXPIRE || '1 day',
     }
 }
