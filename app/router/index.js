b2eb22ec143a305b7723f496c42166ca73b18255
Gray Hound <admin@devilmaydie.name>
2019-10-01 18:28:48 +0500

    A little bit of renaming

--- app/router/index.js
+++ app/router/index.js
@@ -1,11 +1,11 @@
 import RouterApi from './api/index';
-import { RouterBasics } from 'shadow-core-basic';
+import { RouterErrors } from 'shadow-core-basic';
 
 export default function(app) {
   const routerApi = RouterApi(app);
 
   app.express.use('/api/v1', routerApi);
 
-  RouterBasics(app);
+  RouterErrors(app);
 };
 
